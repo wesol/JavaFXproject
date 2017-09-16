6e10d7e92a1d9849aeda498ab29364ab9802a387
maciekwu <mck.wojcik@gmail.com>
2017-09-16 15:08:34 +0200

    C02 dodana tabela Opiekunowie, Produkty

--- trzy_create.sql
+++ trzy_create.sql
@@ -1,5 +1,8 @@
 create database trzy;
 use trzy;
+drop table Klienci;
+#drop table Klienci;
+drop table Opiekunowie;
 
 create table Klienci (
     id_k INT PRIMARY KEY AUTO_INCREMENT,
@@ -11,5 +14,17 @@ create table Klienci (
 );
 
 
+create table Opiekunowie (
+    id_o INT PRIMARY KEY AUTO_INCREMENT,
+    imie VARCHAR(25) NOT NULL,
+    nazwisko VARCHAR(35) NOT NULL,
+    id_k int NOT NULL
+);
+
+
 create table Produkty(
+    id_p INT PRIMARY KEY AUTO_INCREMENT,
+    nazwa VARCHAR(25) NOT NULL,
+	cena int NOT NULL,
+    id_o int NOT NULL
 );
