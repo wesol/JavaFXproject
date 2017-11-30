263841ecd2f6f6c3502d10072bc3d0e8812e73b5
wesol <mwesolowski88@gmail.com>
2017-11-30 21:27:55 +0100

    test

--- src/app/Controller/StatKursanciController.java
+++ src/app/Controller/StatKursanciController.java
@@ -22,7 +22,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 
-public class ListaStatController {
+public class StatKursanciController {
 
 	DBConnector db;
 	public ObservableList<Stat> data;
@@ -75,21 +75,24 @@ public class ListaStatController {
 		Connection conn = db.Connection();
 		data = FXCollections.observableArrayList();
 		String szukaj = tf_filter.getText();
-		ResultSet rs = conn.createStatement().executeQuery(
-				"select kursant.imie, kursant.nazwisko, kursant.edycja, count(statystyki.id), round(sum(statystyki.procent_poprawnych)/count(statystyki.id)) from statystyki join kursant on kursant.login = statystyki.login where locate('"
-						+ szukaj + "',kursant.imie)!=0 or locate('" + szukaj + "',kursant.nazwisko)!=0");
+		if (!szukaj.equals("")) {
+			ResultSet rs = conn.createStatement().executeQuery(
+					"select kursant.imie, kursant.nazwisko, kursant.edycja, count(statystyki.id), round(sum(statystyki.procent_poprawnych)/count(statystyki.id)) from statystyki join kursant on kursant.login = statystyki.login where locate('"
+							+ szukaj + "',kursant.imie)!=0 or locate('" + szukaj + "',kursant.nazwisko)!=0");
 
-		while (rs.next()) {
-			data.add(new Stat(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getDouble(5)));
-		}
+			while (rs.next()) {
+				data.add(new Stat(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getDouble(5)));
 
-		col_imie.setCellValueFactory(new PropertyValueFactory<Stat, String>("name"));
-		col_nazwisko.setCellValueFactory(new PropertyValueFactory<Stat, String>("last"));
-		col_grupa.setCellValueFactory(new PropertyValueFactory<Stat, String>("group"));
-		col_liczbaTest.setCellValueFactory(new PropertyValueFactory<Stat, Integer>("ilosc"));
-		col_sredni.setCellValueFactory(new PropertyValueFactory<Stat, Double>("poprawne"));
-		table_Stat.setItems(null);
-		table_Stat.setItems(data);
+				col_imie.setCellValueFactory(new PropertyValueFactory<Stat, String>("name"));
+				col_nazwisko.setCellValueFactory(new PropertyValueFactory<Stat, String>("last"));
+				col_grupa.setCellValueFactory(new PropertyValueFactory<Stat, String>("group"));
+				col_liczbaTest.setCellValueFactory(new PropertyValueFactory<Stat, Integer>("ilosc"));
+				col_sredni.setCellValueFactory(new PropertyValueFactory<Stat, Double>("poprawne"));
+				table_Stat.setItems(null);
+				table_Stat.setItems(data);
+			}
+		} else
+			initialize();
 
 	}
 
@@ -144,32 +147,33 @@ public class ListaStatController {
 		table_StatPom.setItems(data_pom);
 
 		ResultSet rs_pomWykres = conn.createStatement().executeQuery(
-				"select concat(kursant.imie, ' ', kursant.nazwisko), round(sum(statystyki.procent_poprawnych)/count(statystyki.id)) \n" + 
-				"from statystyki join kursant on kursant.login = statystyki.login where locate('" + szukaj_pom + "',kursant.imie)!=0 or locate('" + szukaj_pom + "',kursant.nazwisko)!=0;");
+				"select kursant.imie, kursant.nazwisko, kursant.edycja, count(statystyki.id), round(sum(statystyki.procent_poprawnych)/count(statystyki.id)) from statystyki join kursant on kursant.login = statystyki.login where locate('"
+						+ szukaj_pom + "',kursant.imie)!=0 or locate('" + szukaj_pom
+						+ "',kursant.nazwisko)!=0 group by kursant.imie, kursant.nazwisko, kursant.edycja");
 
 		while (rs_pomWykres.next()) {
 			series.getData().add(new XYChart.Data<>(rs_pomWykres.getString(1), rs_pomWykres.getDouble(2)));
 			System.out.println(rs_pomWykres.getString(1));
 		}
-		
+
 		ResultSet srednie = conn.createStatement().executeQuery(
 				"select 'œrednie wszystkich uczestników', round(sum(procent_poprawnych)/count(id)) from statystyki;");
 		while (srednie.next()) {
 			series.getData().add(new XYChart.Data<>(srednie.getString(1), srednie.getDouble(2)));
 		}
-		
+
 		bar_statPom.getData().clear();
 		bar_statPom.getData().add(series);
 
 		conn.close();
 	}
-	
+
 	@FXML
 	public void buttonExit(MouseEvent event) throws IOException {
 		Pomocnicze przejdz = new Pomocnicze();
 		przejdz.okno("/app/View/AdminView.fxml", "Panel Egzaminatora", event);
 	}
-	
+
 	@FXML
 	public void initialize() throws SQLException {
 		db = new DBConnector();
